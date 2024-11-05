68c59d177de7dc8536145d98761f0c6acc4f7dcc
SOLOVE-IT~ <solove_biz@yahoo.com>
2024-11-05 18:20:49 +0100

    first commit

---
+++ main.js
@@ -0,0 +1,12 @@
+//import express
+const express = require('express')
+const app = express()
+const port = 2008;
+
+
+
+
+
+
+// listen on port 2008
+app.listen(port, () => console.log(`Listening on port ${port}`));
